b5960e06f82330d31d17f0b4b2e225ea7eb3788e
SondosMatahen <sondosmatahen@gmail.com>
2020-09-29 16:14:24 +0300

    edit eslint

--- server/server.js
+++ server/server.js
@@ -16,42 +16,42 @@ let clientPool = {};
 
 //On new connections, add the client to the connection pool
 server.on('connection', (socket) => {
-    console.log('user seccessfuly connect ');
-    const id = `Socket-${uuid()}`;
-    clientPool[id] = socket;
+  console.log('user seccessfuly connect ');
+  const id = `Socket-${uuid()}`;
+  clientPool[id] = socket;
 
 
-    //Read and parse the incoming data/payload
-    //Verify that the data is legitimate
-    socket.on('data', buffer => {
-        // console.log('data/payload--------->', buffer);
-        logger(buffer);
+  //Read and parse the incoming data/payload
+  //Verify that the data is legitimate
+  socket.on('data', buffer => {
+    // console.log('data/payload--------->', buffer);
+    logger(buffer);
 
 
-    });
+  });
 
-    socket.on('error', e => {
-        console.log('error--------->', e);
-    });
+  socket.on('error', e => {
+    console.log('error--------->', e);
+  });
 
-    socket.on('close', () => {
-        delete socket[id];
-    });
+  socket.on('close', () => {
+    delete socket[id];
+  });
 
 });
 
 function logger(buffer) {
-    let massage = JSON.parse(buffer.toString());
-    const time = new Date();
-    console.log('msg=======>', massage, time);
-    broadcast(massage);
+  let massage = JSON.parse(buffer.toString());
+  const time = new Date();
+  console.log('msg=======>', massage, time);
+  broadcast(massage);
 }
 
 
 
 function broadcast(massage) {
-    let payload = JSON.stringify(massage);
-    for (let id in clientPool) {
-        clientPool[id].write(payload);
-    };
-};
+  let payload = JSON.stringify(massage);
+  for (let id in clientPool) {
+    clientPool[id].write(payload);
+  }
+}
